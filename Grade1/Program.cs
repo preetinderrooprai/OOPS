1b7210a39a693302d0d7bfccf42b0d68352de0df
Preetinder Rooprai <preetinder.rooprai@gmail.com>
2024-10-02 23:30:16 +0530

    Setting up repo

---
+++ Grade1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grade1;
+
+Console.WriteLine("Hello, World!");
+Simple sp = new Simple();
---
+++ Grade1/Simple.cs
@@ -0,0 +1,6 @@
+namespace Grade1;
+public class Simple
+{
+    public Simple() => Console.WriteLine("Simple Class Object Created");
+
+}

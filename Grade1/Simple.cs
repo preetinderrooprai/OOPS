1e631325722ff237b991e7cbc920cd367e245c81
Preetinder Rooprai <preetinder.rooprai@gmail.com>
2024-10-02 23:35:27 +0530

    Changes test repo

--- Grade1/Simple.cs
+++ Grade1/Simple.cs
@@ -1,6 +1,6 @@
 namespace Grade1;
 public class Simple
 {
-    public Simple() => Console.WriteLine("Simple Class Object Created");
+    public Simple() => Console.WriteLine("Simple Class Object Created!!");
 
 }
